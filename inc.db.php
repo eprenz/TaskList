d666578ad607a25677e7bb54eb7c49c9b5ffe6f8
Edward Prenzler <31870444+eprenz@users.noreply.github.com>
2020-12-14 22:40:25 -0600

    Add files via upload

---
+++ inc.db.php
@@ -0,0 +1,10 @@
+<?php
+    // File: inc.db.php
+
+    const HOST = 'localhost';
+    const USER = 'root';
+    const PWD = 'root';
+    const DB = 'project';
+
+    const CONNECT_MYSQL = 'mysql:host=' . HOST . ';dbname=' . DB;
+?>
